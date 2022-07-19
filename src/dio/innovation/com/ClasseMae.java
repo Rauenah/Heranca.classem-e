218f9fb18697deb86a5fbee5692fc9e5b4d11fcc
rauenah <rauena.silvasousa@gmail.com>
2022-07-18 22:57:33 -0300

    Excercicio 2 Classe Mãe, POO.

---
+++ src/dio/innovation/com/ClasseMae.java
@@ -0,0 +1,14 @@
+package dio.innovation.com;
+
+public class ClasseMae {
+    void metodo1 () {
+
+        System.out.println("Metodo 1 de Classe mãe:");
+    }
+    void metodo2 () {
+      System.out.println("Método 1 de Classe mãe");
+
+    }
+}
+
+
